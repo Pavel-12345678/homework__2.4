960ff3a8b2c37c3cf7574d58dcde4ce518f2c36e
Pavel-12345678 <p1980n2014@yandex.ru>
2024-03-02 22:57:45 +0300

    first commit

---
+++ script__2.4.js
@@ -0,0 +1,38 @@
+
+let i = 0;
+while (i < 2) {
+    console.log(i);
+    i++;
+}
+
+for(let i__1 = 2; i__1 <= 5; i__1++) {
+    console.log(i__1);
+}
+
+let i__2 = 2;
+while (i__2 <= 22) {
+console.log(i__2);    
+}
+
+let obj = {
+    "Kоля" : "200",
+    "Вася" : "300",
+    "Петя" : "400"
+};
+for(let key in obj) {
+    console.log(`${key} - зарплата ${obj[key]} - долларов`);
+}
+
+let n = 1000;
+let num = 0;
+while (n > 50) {
+    n /= 2;
+    num++;
+}
+console.log(n);
+console.log(num);
+
+
+for(let firstFriday = 5; firstFriday <= 31; firstFriday++) {
+    console.log(`Сегодня пятница, ${firstFriday} - е число. Необходимо подготовить отчет!`);
+}
